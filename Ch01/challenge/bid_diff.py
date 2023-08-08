c913f15db1a6dbd24994dd3f9a9a1e29a59e7899
AndrewDataSci <98615212+AndrewDataSci@users.noreply.github.com>
2023-08-08 08:33:50 -0400

    Update bid_diff.py

--- Ch01/challenge/bid_diff.py
+++ Ch01/challenge/bid_diff.py
@@ -1,3 +1,5 @@
+# Find the median betweeen the highest price and the 2nd highest price for each bid
+
 import pandas as pd
 
 
@@ -16,9 +18,11 @@ def second(values):
     return second
 
 
-def median_diff(csv_file):
+def median_diff(csv_file): #get the name of the CSV file.
     df = pd.read_csv(csv_file)
-    top1 = df.groupby('id')['price'].max()
-    top2 = df.groupby('id')['price'].apply(second)
-    diffs = top1 - top2
-    return diffs.median()
+    top1 = df.groupby('id')['price'].max() #group by the ID and get the maximum price
+    top2 = df.groupby('id')['price'].apply(second) #group by ID and get the second to maximum price as per function above
+    diffs = top1 - top2 # calculate the difference
+    return diffs.median() #return the median between the differences.
+
+#Find out where the code is spedning it's time and use bods.csv.xz for data
